ae558cd53cb4babcb33dd39141c0d5b52582d25b
xiao_51 <972766740@qq.com>
2017-05-10 02:30:50 +0800

    name

--- pages/home/home.js
+++ pages/home/home.js
@@ -4,31 +4,39 @@ const app = getApp();
 //var fanQuery = new AV.Query('fans');
 
 Page({
-    data: {
-        user: null,
-        currtBtn: 0,
-        userInfo: null
-    },
-    onLoad: function () {
-        var that = this;
-        that.data.user = User.current().toJSON();
-        console.log(this.data.user);
-        //User.current().set('username','刘五一');
-        //User.current().save();
-        console.log(User.current().id);
-    },
-    btnClick: function (e) {
-        switch (e.currentTarget.id) {
-            case "each":
-                this.setData({ currtBtn: 0 });
-                //this.saveFan();
-                break;
-            case "noteach":
-                this.setData({ currtBtn: 1 });
-                this.saveFan();
-                break;
-        }
-    },
-    saveFan: function () {
+  data: {
+    user: null,
+    currtBtn: 0,
+    userInfo: null
+  },
+  onLoad: function () {
+    var that = this;
+    that.data.user = User.current().toJSON();
+    console.log(this.data.user);
+    //User.current().set('realname','刘五一');
+    //User.current().save();
+    console.log(that.data.user.realname);
+
+    console.log(User.current().id);
+  },
+  btnClick: function (e) {
+    var that = this;
+    if (that.data.user.realname) {
+      wx.switchTab({
+        url: '../user/user'
+      })
     }
+    switch (e.currentTarget.id) {
+      case "each":
+        this.setData({ currtBtn: 0 });
+        //this.saveFan();
+        break;
+      case "noteach":
+        this.setData({ currtBtn: 1 });
+        this.saveFan();
+        break;
+    }
+  },
+  saveFan: function () {
+  }
 })
